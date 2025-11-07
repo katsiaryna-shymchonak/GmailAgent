d7c7bdfaeafaabb455e6042676eec2eac672055c
Ekaterina <katsiaryna.shymchonak@gmail.com>
2025-11-07 12:34:18 +0300

    Fixed npm and lint use

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginImport from 'eslint-plugin-import';
+import pluginN from 'eslint-plugin-n';
+import pluginPromise from 'eslint-plugin-promise';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: [
+      'node_modules/',
+      '**/dist/**',
+      '**/.github/**',
+      '**/build/**',
+      'eslint.config.js'
+    ]
+  },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'script',
+      globals: {
+        ...globals.browser,
+        chrome: 'readonly'
+      }
+    },
+    plugins: {
+      import: pluginImport,
+      n: pluginN,
+      promise: pluginPromise
+    },
+    rules: {
+      'no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrors: 'none' }
+      ],
+      'no-console': 'off',
+      'import/no-unresolved': 'off',
+      'n/no-missing-import': 'off'
+    }
+  },
+  prettier
+];
